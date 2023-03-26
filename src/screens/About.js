abb696865524e7a222475979abf918102f409dbd
Ruddy marc <pc-de-ruddy@LAPTOP-OONQKG13.gp>
2023-03-26 18:39:20 -0400

    add about page

---
+++ src/screens/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Show about page
+ * @returns 
+ */
+export default function About() {
+  return (
+    <>
+      <h2>Informations</h2>
+      <p>This page show most informations about us.</p>
+    </>
+  )
+}
