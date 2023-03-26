a14ddd12ade649ca83fabb6f545f3eaf847bcac4
Ruddy marc <pc-de-ruddy@LAPTOP-OONQKG13.gp>
2023-03-26 19:21:56 -0400

    linked users path to user list element

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 import Layout from './screens/Layout';
 import Home from './screens/Home';
 import About from './screens/About';
+import UserList from './screens/UserList';
 import UnknownRoute from './screens/UnkownRoute';
 
 export default createBrowserRouter([
@@ -12,6 +13,7 @@ export default createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: 'infos', element: <About /> },
+      { path: 'users', element: <UserList /> },
       { path: '*', element: <UnknownRoute /> },
     ]
   },
