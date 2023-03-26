f64e0398f116894bbff9fdf2be87eff6b1cb8379
Ruddy marc <pc-de-ruddy@LAPTOP-OONQKG13.gp>
2023-03-26 17:39:22 -0400

    add home page

---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Show home page content
+ * @returns 
+ */
+export default function Home() {
+  return (
+    <>
+      <h2>Home page</h2>
+      <p>Welcome to home page</p>
+    </>
+  )
+}
